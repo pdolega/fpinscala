e113baf36ad2276dbe3a9ee08f452dba5fa65ee6
Paweł Dolega <pdolega@nexelem.com>
2015-01-03 20:29:07 +0100

    Chapter 3 ex. 28 & 29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,9 +6,9 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-  def size[A](tree: Tree[A]): Int = tree match {
+  def count[A](tree: Tree[A]): Int = tree match {
     case Leaf(_) => 1
-    case Branch(left, right) => size(left) + size(right)
+    case Branch(left, right) => count(left) + count(right)
   }
 
   def max(tree: Tree[Int]): Int = tree match {
@@ -19,5 +19,15 @@ object Tree {
   def depth[A](tree: Tree[A]): Int = tree match {
     case Leaf(_) => 1
     case Branch(left, right) => (depth(left) + 1) max (depth(right) + 1)
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(value) => Leaf(f(value))
+    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+  }
+
+  def fold[A, B](tree: Tree[A])(f: A => B)(combine: (B, B) => B): B = tree match {
+    case Leaf(value) => f(value)
+    case Branch(left, right) => combine(fold(left)(f)(combine), fold(right)(f)(combine))
   }
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -10,11 +10,19 @@ import org.scalatest.junit.JUnitRunner
  */
 @RunWith(classOf[JUnitRunner])
 class TreeSpec extends WordSpec {
+
+  final val ExtensiveTree = Branch(
+    Leaf(1), Branch(
+      Branch(
+        Leaf(1), Branch(
+          Leaf(17), Leaf(15))),
+      Leaf(2)))
+  
   "Tree" should {
-    "have size working properly on simple trees" in {
-      assert(size(Leaf(1)) == 1)
-      assert(size(Branch(Leaf(1), Leaf(2))) == 2)
-      assert(size(Branch(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), Leaf(4))) == 4)
+    "have count working properly on simple trees" in {
+      assert(count(Leaf(1)) == 1)
+      assert(count(Branch(Leaf(1), Leaf(2))) == 2)
+      assert(count(Branch(Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), Leaf(4))) == 4)
     }
 
     "have max work as expected" in {
@@ -22,7 +30,7 @@ class TreeSpec extends WordSpec {
       assert(max(Branch(Leaf(5), Leaf(5))) == 5)
       assert(max(Branch(Branch(Leaf(1), Branch(Leaf(7), Leaf(2))), Leaf(3))) == 7)
     }
-
+    
     "have depth working as expected" in {
       assert(depth(Leaf(5)) == 1)
 
@@ -31,13 +39,54 @@ class TreeSpec extends WordSpec {
           Leaf(1), Leaf(2)
         ))) == 3)
 
-      assert(depth(Branch(
-        Leaf(1), Branch(
+      assert(depth(ExtensiveTree) == 5)
+    }
+
+    "have map working correctly" in {
+      assert(map(Leaf(5)){ _.toString } == Leaf("5"))
+
+      assert(map(ExtensiveTree) { _.toDouble + 0.5 } ==
+        Branch(
+          Leaf(1.5), Branch(
+            Branch(
+              Leaf(1.5), Branch(
+                Leaf(17.5), Leaf(15.5))),
+            Leaf(2.5)))
+      )
+    }
+  }
+
+  "all above reimplemented with fold" should {
+    "work for count" in {
+      assert(fold(Leaf(5)) { x => 1 } { _ + _ } == 1 )
+      assert(fold(ExtensiveTree) { x => 1 } { _ + _ } == 5 )
+    }
+
+    "work for max" in {
+      assert(fold(Leaf(5)) { x => x } { _ max _ } == 5 )
+      assert(fold(ExtensiveTree) { x => x } { _ max _ } == 17 )
+    }
+
+    "work for depth" in {
+      assert(fold(Leaf(5)) { x => 1 } { (x, y) => (x + 1) max (y + 1) } == 1 )
+      assert(fold(ExtensiveTree) { x => 1 } { (x, y) => (x + 1) max (y + 1) } == 5 )
+    }
+
+    "work for map" in {
+      assert(
+        fold(Leaf(5)) { x => Leaf(x.toString) : Tree[String] } { Branch(_, _) } == Leaf("5")
+      )
+
+      assert(
+        fold(ExtensiveTree) { x => Leaf(x.toDouble + 0.5): Tree[Double] } { Branch(_, _) }
+          ==
           Branch(
-            Leaf(1), Branch(
-              Leaf(17), Leaf(15))),
-          Leaf(2)))
-      ) == 5)
+            Leaf(1.5), Branch(
+              Branch(
+                Leaf(1.5), Branch(
+                  Leaf(17.5), Leaf(15.5))),
+              Leaf(2.5)))
+      )
     }
   }
 }
